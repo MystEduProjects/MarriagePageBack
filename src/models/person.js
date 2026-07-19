91c05d5639c36b021182971b09a9bb873e73def1
Eduardo <esep@uc.cl>
2026-07-18 22:19:22 -0400

    add escort allergies

--- src/models/person.js
+++ src/models/person.js
@@ -5,12 +5,13 @@ const Schema = mongoose.Schema;
 const personSchema = new Schema({
   name: String,
   attend: Boolean,
+  menu: String,
+  allergies: String,
   availableEscort: Boolean,
   wantsEscort: Boolean,
   escort: String,
   escortMenu: String,
-  menu: String,
-  allergies: String
+  escortAllergies: String
 });
 
 const Person = mongoose.model('Person', personSchema);
